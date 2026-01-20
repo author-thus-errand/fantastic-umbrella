d22bc2eb074a1a814572d226b9d3188976590bf8
author-thus-errand <author-thus-errand@duck.com>
2026-01-20 09:58:43 +0800

    Implement link replacement for Twitter/X in messages
    Added functionality to replace Twitter/X links in messages.

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,42 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { before } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
+
+let unpatches: Function[] = [];
+
+const replaceLinks = (content: string): string => {
+  return content.replace(
+    /(https?:\/\/)(?:mobile\.)?(twitter\.com|x\.com)\/([A-Za-z0-9_]+)\/status\/(\d+)(\/[a-z]+\/\d+)?(\?.*)?/gi,
+    (match, protocol, domain, username, statusId, mediaPath = "", query = "") => {
+      const fixerDomain = domain.includes("twitter") ? "fxtwitter.com" : "fixupx.com";
+      return `${protocol}${fixerDomain}/${username}/status/${statusId}${mediaPath}${query}`;
+    }
+  );
+};
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+  onLoad: () => {
+    const MessageActions = findByProps("sendMessage", "editMessage");
+    if (!MessageActions) return;
+
+    unpatches.push(
+      before("sendMessage", MessageActions, (args) => {
+        const [, message] = args;
+        if (message?.content) {
+          message.content = replaceLinks(message.content);
+        }
+      })
+    );
+
+    unpatches.push(
+      before("editMessage", MessageActions, (args) => {
+        const [, , message] = args;
+        if (message?.content) {
+          message.content = replaceLinks(message.content);
+        }
+      })
+    );
+  },
+  onUnload: () => {
+    unpatches.forEach((unpatch) => unpatch());
+  },
+};
